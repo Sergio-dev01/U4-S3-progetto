884bc2065349c4957cc8f8cc14dce2805aa4a27b
Sergio-dev01 <sergio.develoop@gmail.com>
2025-07-04 12:21:54 +0200

    add mod class

---
+++ src/main/java/sergiomaselli/entities/Prestito.java
@@ -0,0 +1,101 @@
+package sergiomaselli.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Prestito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id", nullable = false)
+    private Utente utente;
+
+
+    @ManyToOne
+    @JoinColumn(name = "catalogo_isbn", nullable = false)
+    private Catalogo elementoPrestato;
+
+    private LocalDate dataInizioPrestito;
+
+    private LocalDate dataRestituzionePrevista;
+
+    private LocalDate dataRestituzioneEffettiva;
+
+    public Prestito() {
+    }
+
+    public Prestito(Utente utente, Catalogo elementoPrestato, LocalDate dataRestituzionePrevista, LocalDate dataInizioPrestito, LocalDate dataRestituzioneEffettiva) {
+        this.utente = utente;
+        this.elementoPrestato = elementoPrestato;
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+        this.dataInizioPrestito = dataInizioPrestito;
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public Catalogo getElementoPrestato() {
+        return elementoPrestato;
+    }
+
+    public void setElementoPrestato(Catalogo elementoPrestato) {
+        this.elementoPrestato = elementoPrestato;
+    }
+
+    public LocalDate getDataInizioPrestito() {
+        return dataInizioPrestito;
+    }
+
+    public void setDataInizioPrestito(LocalDate dataInizioPrestito) {
+        this.dataInizioPrestito = dataInizioPrestito;
+        if (dataInizioPrestito != null) {
+            this.dataRestituzionePrevista = dataInizioPrestito.plusDays(30);
+        }
+    }
+
+    public LocalDate getDataRestituzionePrevista() {
+        return dataRestituzionePrevista;
+    }
+
+    public void setDataRestituzionePrevista(LocalDate dataRestituzionePrevista) {
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+    }
+
+    public LocalDate getDataRestituzioneEffettiva() {
+        return dataRestituzioneEffettiva;
+    }
+
+    public void setDataRestituzioneEffettiva(LocalDate dataRestituzioneEffettiva) {
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestito{" +
+                "id=" + id +
+                ", utente=" + utente +
+                ", elementoPrestato=" + elementoPrestato +
+                ", dataInizioPrestito=" + dataInizioPrestito +
+                ", dataRestituzionePrevista=" + dataRestituzionePrevista +
+                ", dataRestituzioneEffettiva=" + dataRestituzioneEffettiva +
+                '}';
+    }
+}
--- src/main/java/sergiomaselli/entities/Utente.java
+++ src/main/java/sergiomaselli/entities/Utente.java
@@ -1,4 +1,73 @@
 package sergiomaselli.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class Utente {
+
+    @Id
+    private int numeroTessera;
+
+    private String nome;
+    private String cognome;
+    private LocalDate dataNascita;
+
+    @OneToMany(mappedBy = "utente")
+    private List<Prestito> prestiti;
+
+    public Utente() {
+    }
+
+    public Utente(int numeroTessera, String nome, String cognome, LocalDate dataNascita) {
+        this.numeroTessera = numeroTessera;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+    }
+
+    public int getNumeroTessera() {
+        return numeroTessera;
+    }
+
+    public void setNumeroTessera(int numeroTessera) {
+        this.numeroTessera = numeroTessera;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "numeroTessera=" + numeroTessera +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", dataNascita=" + dataNascita +
+                '}';
+    }
 }
